6693004a5595e0bffd4d95f9c1ad6accfc996675
windog <windog.dev@gmail.com>
2016-06-28 22:23:35 +0800

    Test FetchWeather

--- app/src/main/java/com/windy/udacity/mysunshine/ForecastFragment.java
+++ app/src/main/java/com/windy/udacity/mysunshine/ForecastFragment.java
@@ -2,12 +2,9 @@ package com.windy.udacity.mysunshine;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,19 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
 
 /**
  * Created by windog on 2016/6/1.
@@ -117,7 +102,7 @@ public class ForecastFragment extends Fragment {
     /* 刷新的操作封装为一个方法
     * */
     private void updateWeather() {
-        FetchWeatherTask weatherTask = new FetchWeatherTask();
+        FetchWeatherTask weatherTask = new FetchWeatherTask(getActivity());
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String location = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
         weatherTask.execute(location);
@@ -130,191 +115,5 @@ public class ForecastFragment extends Fragment {
         super.onStart();
         updateWeather();
     }
-
-
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-
-        private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
-            long roundedHigh = Math.round(high);
-            long roundedLow = Math.round(low);
-            String highLowStr = roundedHigh + "/" + roundedLow;
-            return highLowStr;
-        }
-
-        public String[] getWeatherDataFromJson(String forecastJsonStr) {
-
-            // 固定一周的数组
-            String[] resultsStrs = new String[7];
-            // Gson解析
-            Gson gson = new Gson();
-            WeatherInfo weatherInfo = gson.fromJson(forecastJsonStr, WeatherInfo.class);
-
-
-            // For now, using the format "Day - description - hi/low"
-            for (int i = 0; i < 7; i++) {
-                // 日期
-                String day;
-                // 天气描述
-                String description;
-                // 最高温与最低温拼接成的字符，like high/low
-                String highAndLow;
-
-                List<WeatherInfo.ListBean> list = weatherInfo.getList();
-                WeatherInfo.ListBean listBean = list.get(i);
-                List<WeatherInfo.ListBean.WeatherBean> weatherBeanList = listBean.getWeather();
-
-                // parse out the excepted data
-                // dayTime : Unix timestamp is a long
-                int dayTime = listBean.getDt();
-                description = weatherBeanList.get(0).getDescription();
-                double max = listBean.getTemp().getMax();
-                double min = listBean.getTemp().getMin();
-
-                // for debugging if my data are right
-//                Log.d(LOG_TAG , "dt :" + listBean.getDt() );
-//                Log.d(LOG_TAG , "description " + weatherBeanList.get(0).getDescription() );
-//                Log.d(LOG_TAG , "max :" + listBean.getTemp().getMax() );
-//                Log.d(LOG_TAG , "min :" + listBean.getTemp().getMin() );
-
-                // 日期格式转换
-                //create a Gregorian Calendar, which is in current date
-                GregorianCalendar gc = new GregorianCalendar();
-                //add i dates to current date of calendar
-                gc.add(GregorianCalendar.DATE, i);
-                //get that date, format it, and "save" it on variable day
-                Date time = gc.getTime();
-                SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-                day = shortenedDateFormat.format(time);
-//                Log.d(LOG_TAG,"day :" + day);
-
-
-                highAndLow = formatHighLows(max, min);
-                resultsStrs[i] = day + " - " + description + " - " + highAndLow;
-            }
-
-            for (String s : resultsStrs) {
-                Log.d(LOG_TAG, "foreccast result: " + s);
-            }
-            return resultsStrs;
-        }
-
-        /*  String... params 相当于 String[] params 传了个 String 类型数组进去
-            此方法是另开线程执行的
-        * */
-        @Override
-        protected String[] doInBackground(String... params) {
-            // 判断传入的参数数组是否为空
-            if (params.length == 0) {
-                return null;
-            }
-
-            // These two need to be declared outside the try/catch
-            // so that they can be closed in the finally block.
-            HttpURLConnection urlConnection = null;
-            BufferedReader reader = null;
-
-            // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
-
-            String cityName = "beijing";
-
-            try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
-                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?";
-
-                final String QUERY_PARAM = "q";
-                final String APPID_PARAM = "APPID";
-
-                // buildUpon 在。。。基础上构建
-                Uri builtUri = Uri.parse(baseUrl).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, cityName)
-                        .appendQueryParameter(APPID_PARAM, BuildConfig.OPEN_WEATHER_MAP_API_KEY)
-                        .build();
-
-                URL url = new URL(builtUri.toString());
-                // print integrated URL
-//                Log.d(LOG_TAG, "Built URI is :" + builtUri.toString());
-
-                // Create the request to OpenWeatherMap, and open the connection
-                urlConnection = (HttpURLConnection) url.openConnection();
-                urlConnection.setRequestMethod("GET");
-                urlConnection.connect();
-
-                // Read the input stream into a String
-                InputStream inputStream = urlConnection.getInputStream();
-                StringBuffer buffer = new StringBuffer();
-                if (inputStream == null) {
-                    // Nothing to do.
-                    return null;
-                }
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                String line;
-                while ((line = reader.readLine()) != null) {
-                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
-                    // But it does make debugging a *lot* easier if you print out the completed
-                    // buffer for debugging.
-                    buffer.append(line + "\n");
-                }
-
-                if (buffer.length() == 0) {
-                    // Stream was empty.  No point in parsing.
-                    return null;
-                }
-                forecastJsonStr = buffer.toString();
-//                Log.d(LOG_TAG, "Returned string from OPEN_WEATHER: " + forecastJsonStr);
-
-
-            } catch (IOException e) {
-                Log.e(LOG_TAG, "Error ", e);
-                // If the code didn't successfully get the weather data, there's no point in attemping
-                // to parse it.
-                return null;
-            } finally {
-                if (urlConnection != null) {
-                    urlConnection.disconnect();
-                }
-                if (reader != null) {
-                    try {
-                        reader.close();
-                    } catch (final IOException e) {
-                        Log.e(LOG_TAG, "Error closing stream", e);
-                    }
-                }
-            }
-
-            try {
-                return getWeatherDataFromJson(forecastJsonStr);
-            } catch (Exception e) {
-                Log.e(LOG_TAG, e.getMessage(), e);
-                e.printStackTrace();
-            }
-
-            // This will only happen if there was an error getting or parsing the forecast.
-            return null;
-        }
-
-        /* 此方法已回到主线程，接收的是 doInBackground() 的返回值
-        * */
-        @Override
-        protected void onPostExecute(String[] result) {
-            if (result != null) {
-                mForecastAdapter.clear();
-                mForecastAdapter.addAll(result);
-                // 下面这个写法与 addAll 相同
-//                for(String dayForecastStr : result) {
-//                    mForecastAdapter.add(dayForecastStr);
-//                }
-                // New data is back from the server.  Hooray!
-            }
-        }
-    }
-
 
 }
